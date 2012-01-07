25505dc1b1587687810ed8d8da6915e295c04a76
Jack Armley <jack_armley@yahoo.co.uk>
2012-01-07 23:39:51 +0000

    refimenents

--- scripts/snowcone.js
+++ scripts/snowcone.js
@@ -7,7 +7,7 @@ var icons = {
 var browserver = parseInt($.browser.version, 10)
 
 $(document).ready(function(){
-	if ($.browser.msie && browserver == 6 || browserver == 7 || browserver == 8){
+	if ($.browser.msie && browserver == 6 || browserver == 7){
 		$(".app-ico").each(function(){
 			var icon = $(this).attr("class").split(/\s/);
 			for(key in icons){
