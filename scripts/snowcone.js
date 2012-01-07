38478c7ff2ea3779ae610610534830cf527074e4
Jack Armley <jack_armley@yahoo.co.uk>
2012-01-07 23:29:49 +0000

    trying out an ie fix

--- scripts/snowcone.js
+++ scripts/snowcone.js
@@ -1,6 +1,17 @@
 //Script to ensure Snowcone will work for legacy browsers, that don't support the :before pseudo class
+
+var icons = {
+    ico_alert: "A",
+    ico_cross: "B"
+};
+
 $(document).ready(function(){
-	if($.browser.msie){
-		$(".app-ico").prepend("<span class='ico-legacy'>C</span>");
-	}
+	$(".app-ico").each(function(){
+		var icon = $(this).attr("class").split(/\s/);
+		for(key in icons){
+			if([key]==icon[1]){
+				$(this).prepend("<span class='app-legacy_ico'>" + icons[key] + "</span>");
+			};
+		}
+	});
 });
