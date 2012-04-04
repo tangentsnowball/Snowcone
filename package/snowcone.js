9bfb12f7060675b1aa6762780c31a00b2b8530bc
Jack Armley <jack_armley@yahoo.co.uk>
2012-04-04 16:56:20 +0100

    Added Jade's shiny new icons to make snowcone a nice neat. angular font where everything is more consistent!

--- package/snowcone.js
+++ package/snowcone.js
@@ -1,46 +1,63 @@
-//Script to ensure Snowcone will work for legacy browsers, that don't support the :before pseudo class
+//Script to ensure Snowcone will work for legacy browsers, that don't support the " pseudo class
 
 function addicons(){
 	//Create object with list of icons
 		var icons = {
 			"sc-alert":"\f000",
-			"sc-cross":"\f001",
-			"sc-tick":"\f002",
-			"sc-expand":"\f003",
+		    "sc-cross":"\f001",
+			"sc-expand":"\f002",
+			"sc-tick":"\f003",
 			"sc-contract":"\f004",
-			"sc-view":"\f005",
-			"sc-sync":"\f006",
-			"sc-home":"\f007",
-			"sc-outline_down":"\f008",
-			"sc-outline_up":"\f009",
-			"sc-outline_right":"\f00a",
-			"sc-outline_left":"\f00b",
-			"sc-stop":"\f00c",
-			"sc-mapmarker":"\f00d",
-			"sc-favourite":"\f00e",
-			"sc-profile":"\f010",
-			"sc-magnify":"\f011",
-			"sc-comment":"\f012",
-			"sc-settings":"\f013",
-			"sc-edit":"\f014",
-			"sc-email":"\f015",
-			"sc-shop_bag":"\f016",
-			"sc-logout":"\f017",
-			"sc-heart":"\f018", 
-			"sc-mobile":"\f019", 
-			"sc-rss":"\f01a", 
-			"sc-link":"\f01b", 
-			"sc-fill_down":"\f01c", 
-			"sc-fill_up":"\f01d", 
-			"sc-fill_right":"\f01e",
-			"sc-fill_left":"\f021", 
-			"sc-facebook":"\f022", 
-			"sc-twitter":"\f023", 
-			"sc-googleplus":"\f024", 
-			"sc-blacklist":"\f025", 
-			"sc-tag":"\f026", 
-			"sc-speedo":"\f027",  
-			"sc-tangentsnowball":"\f028" 
+			"sc-outline_down":"\f005",
+			"sc-outline_up":"\f006",
+			"sc-outline_right":"\f007",
+			"sc-outline_left":"\f008",
+			"sc-fill_down":"\f009",
+			"sc-fill_top":"\f00a",
+			"sc-fill_right":"\f00b",
+			"sc-fill_left":"\f00c",
+			"sc-profile":"\f00d",
+			"sc-search":"\f00e",
+			"sc-comment":"\f010",
+			"sc-settings":"\f011",
+			"sc-edit":"\f012",
+			"sc-email_closed":"\f013",
+			"sc-email_open":"\f014",
+			"sc-logout":"\f015",
+			"sc-heart":"\f016",
+			"sc-phone":"\f017",
+			"sc-rss":"\f018", 
+			"sc-link":"\f019", 
+			"sc-stop":"\f01a", 
+			"sc-mapmarker":"\f01b", 
+			"sc-favourite":"\f01c", 
+			"sc-fb_fill":"\f01d", 
+			"sc-twitter_fill":"\f01e",
+			"sc-gplus_fill":"\f021", 
+			"sc-profile_blacklist":"\f022", 
+			"sc-tag":"\f023", 
+			"sc-tangentsnowball":"\f024", 
+			"sc-refresh":"\f025", 
+			"sc-upload":"\f026", 
+			"sc-download":"\f027",  
+			"sc-bookmark":"\f028", 
+			"sc-flag":"\f029", 
+			"sc-doc_default":"\f02a", 
+			"sc-info":"\f02b", 
+			"sc-calendar":"\f02c", 
+			"sc-print":"\f02d", 
+			"sc-home":"\f02e", 
+			"sc-view":"\f02f", 
+			"sc-sync":"\f030", 
+			"sc-hcard":"\f031", 
+			"sc-shop_bag":"\f032", 
+			"sc-pants":"\f033", 
+			"sc-pants_granny":"\f034", 
+			"sc-pants_bikini":"\f035", 
+			"sc-pants_speedo":"\f036", 
+			"sc-target":"\f037", 
+			"sc-trash":"\f038", 
+			"sc-notes":"\f039" 
 		};
 	//Detect IE version
 		var browserver = parseInt($.browser.version, 10);
@@ -62,5 +79,8 @@ function addicons(){
 //Run function
 	$(document).ready(function(){   
 		addicons();
-	});
+	});     
+	
+	
+	
 	
