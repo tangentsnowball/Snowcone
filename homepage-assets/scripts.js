3bc3545182c4254197e661271c3a5229dd0f6c5e
Jack Armley <jack_armley@yahoo.co.uk>
2012-04-05 11:48:03 +0100

    Added view switcher to preview

---
+++ homepage-assets/scripts.js
@@ -0,0 +1,23 @@
+(function(window,undefined ) {
+	var homepage = {
+		ui:{
+			toggleIconView: function(){
+				$(".toggle-view li").click(function(){
+					//Set variables
+					var $this = $(this);
+					var toggleClass = $this.attr("class");
+				   //Perform toggle 
+				   $("body")
+						.removeAttr("class")
+						.addClass(toggleClass);
+				});
+			}
+		}
+	} 
+window.homepage = homepage;
+})(window);	  
+	
+
+$(document).ready(function() {
+	homepage.ui.toggleIconView()		 
+});
