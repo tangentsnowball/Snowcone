c1bb5f0edbabeea79f740490e12df3741f43566b
jackarmley <jack_armley@yahoo.co.uk>
2012-06-08 08:57:39 +0800

    Added Creative Commons, Share-alike no-attribution licence info (issue #6)

--- snowcone/vanilla/package/snowcone-vanilla.js
+++ snowcone/vanilla/package/snowcone-vanilla.js
@@ -1,5 +1,11 @@
 // Script to ensure Snowcone will work for legacy browsers, that don't support the pseudo class
 
+//--
+//Snowcone by Tangent Snowball is licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License. Based on a work at github.com.
+//http://creativecommons.org/licenses/by-sa/3.0/"	
+//--
+
+
 function addicons(){
   //Create object with list of icons
   var icons = {
